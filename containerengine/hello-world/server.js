a1002e78b222120e77fef80e67b0276da6af461e
AlvarDev <deivid07.21@gmail.com>
2017-07-01 15:53:21 -0300

    new container

--- containerengine/hello-world/server.js
+++ containerengine/hello-world/server.js
@@ -19,7 +19,7 @@
 var http = require('http');
 var handleRequest = function (req, res) {
   res.writeHead(200);
-  res.end('Hello Kubernetes!');
+  res.end('Acabei de perder $20.00!!!!');
 };
 var www = http.createServer(handleRequest);
 www.listen(process.env.PORT || 8080);
